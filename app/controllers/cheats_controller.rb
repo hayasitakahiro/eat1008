5a414eb3409fbd0da3f544b749bd3ea89acb23bd
hayasitakahiro <0kv1td3.7254p9e@au.com>
2022-08-13 23:59:46 +0900

    Active Storage導入

--- app/controllers/cheats_controller.rb
+++ app/controllers/cheats_controller.rb
@@ -1,8 +1,18 @@
 class CheatsController < ApplicationController
   def index
+    @cheat = Cheat.all
   end
 
 def new
+  @cheat = Cheat.new
 end
 
+def create
+  Cheat.create(cheat_params)
+
+end
+private
+def cheat_params
+  params.require(:cheat).permit(:name, :image)
+end
 end
