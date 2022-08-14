4702a21959be20d8bb3e2c04c08e2cc00e60773b
hayasitakahiro <0kv1td3.7254p9e@au.com>
2022-08-14 21:18:21 +0900

    削除、編集機能実装

--- app/controllers/cheats_controller.rb
+++ app/controllers/cheats_controller.rb
@@ -8,25 +8,42 @@ def new
 end
 
 def create
+
   @cheat = Cheat.new(cheat_params)
   if @cheat.save
     redirect_to root_path
   else
     render :new
 end
-   
+end
+
 def show
+ 
   @cheat = Cheat.find(params[:id])
 end
     
 def destroy
-  tweet = Tweet.find(params[:id])
-  tweet.destroy
+ 
+  @cheat = Cheat.find(params[:id])
+  @cheat.destroy
+  
+  redirect_to root_path
 end
 
 def edit
+  @cheat = Cheat.find(params[:id])
+  redirect_to root_path
 end
+
+def update
+  @cheat = Cheat.find(params[:id])
+  if @cheat.update(cheat_params)
+    redirect_to cheat_path
+  else
+    render :edit
+  end
 end
+
 private
 def cheat_params
   params.require(:cheat).permit(:name, :image)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'cheats/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "cheats#index"
-  resources :cheats, only: [:index, :new, :create, :show, :edit, :destroy]
+  resources :cheats
 end
