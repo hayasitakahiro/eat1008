87a9ba752c4856fe42c21769f7d1d8a5fb648f40
hayasitakahiro <0kv1td3.7254p9e@au.com>
2022-08-19 21:10:52 +0900

    update機能実装

--- app/controllers/cheats_controller.rb
+++ app/controllers/cheats_controller.rb
@@ -23,22 +23,21 @@ class CheatsController < ApplicationController
  end
     
  def destroy
- 
   @cheat = Cheat.find(params[:id])
   @cheat.destroy
-  
   redirect_to root_path
  end
 
  def edit
   @cheat = Cheat.find(params[:id])
-  redirect_to root_path
+  
+  
  end
 
  def update
   @cheat = Cheat.find(params[:id])
    if @cheat.update(cheat_params)
-    redirect_to cheat_path
+    redirect_to root_path
   else
     render :edit
    end
