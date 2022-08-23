68467db92b855794ff290ebace0e36ba579f6038
hayasitakahiro <0kv1td3.7254p9e@au.com>
2022-08-23 18:43:44 +0900

    ログイン機能修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'cheats/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "cheats#index"
+  resources :users, only: :show
   resources :cheats
   
 end
