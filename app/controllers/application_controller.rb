9d4fcc134f927582e34214b8477b70b6d301f028
hayasitakahiro <0kv1td3.7254p9e@au.com>
2022-08-24 19:10:12 +0900

    Basic認証修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
  
   private
@@ -9,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']  # 環境変数を読み込む記述に変更
+    end
   end
-end
 end
